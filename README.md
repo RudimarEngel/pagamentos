6117b31a0f5f18d446ec040c8b7ed9f056c883e3
RudimarEngel <rudimarengel@gmail.com>
2022-08-14 17:02:25 -0300

    procurando alguma alternativa de arquiteturar melhor o código.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # pagamentos
 
 # REFRENCIAS:
-# https://go.dev/doc/tutorial/web-service-gin
-# https://medium.com/wesionary-team/a-clean-architecture-for-web-application-in-go-lang-4b802dd130bb
+- https://go.dev/doc/tutorial/web-service-gin
+- https://medium.com/wesionary-team/a-clean-architecture-for-web-application-in-go-lang-4b802dd130bb
+- https://go.dev/doc/code
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+//	"./pkg"
+)
+
+type transferencia struct {
+	ID		 string `json:"id"`
+	Valor	 string `json:"valor"`
+	IdPagante string `json:"IdPagante"`
+	IdRecebedor	 float64 `json:"IdRecebedor"`
+}
+
+/*var albums = []album {
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+  {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+  {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}*/
+
+func postTransferencia(c *gin.Context) {
+	fmt.Println("AAAAAAAAAAAAAAAAA!")
+	var novaTransferencia transferencia
+
+	if err := c.BindJSON(&novaTransferencia); err != nil {
+		return
+	}
+
+//	L.Demo()
+	
+	c.IndentedJSON(http.StatusCreated, novaTransferencia)
+}
+
+
+func main() {
+	router := gin.Default()
+	// router.GET("/albums", getAlbums)
+	router.POST("/transferencia", postTransferencia)
+
+	router.Run("localhost:8080")
+}
